9626177bc2e26e549225a680763cafb83ec2bbc2
minaj002 <minaj002@gmail.com>
2017-04-13 15:40:02 +0300

    added fake test

---
+++ src/test/java/com/weststein/handler/ApplicationHandlerTest.java
@@ -0,0 +1,15 @@
+package com.weststein.handler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationHandlerTest {
+    @Test
+    public void handle() throws Exception {
+
+        assertTrue(true);
+
+    }
+
+}
