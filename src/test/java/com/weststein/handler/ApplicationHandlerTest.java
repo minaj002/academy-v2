d961290ea316d4eb31de0cce75e6719e243bdf84
minaj002 <minaj002@gmail.com>
2017-04-20 11:03:23 +0300

    strange test behavior

--- src/test/java/com/weststein/handler/ApplicationHandlerTest.java
+++ src/test/java/com/weststein/handler/ApplicationHandlerTest.java
@@ -9,9 +9,6 @@ public class ApplicationHandlerTest {
     public void handle() throws Exception {
 
         assertTrue(true);
-        assertTrue(true);
-        assertTrue(true);
-        assertTrue(true);
 
     }
 
