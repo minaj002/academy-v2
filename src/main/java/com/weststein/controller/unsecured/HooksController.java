066808d657ae291062bc3b54edff1f5772ff97dd
minaj002 <minaj002@gmail.com>
2017-05-11 11:13:36 +0300

    hooks

--- src/main/java/com/weststein/controller/unsecured/HooksController.java
+++ src/main/java/com/weststein/controller/unsecured/HooksController.java
@@ -3,7 +3,6 @@ package com.weststein.controller.unsecured;
 import com.weststein.handler.account.SaveBookingHandler;
 import com.weststein.handler.identification.SavePersonIdentificationHandler;
 import com.weststein.integration.SolarisBooking;
-import com.weststein.integration.SolarisIdentification;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -12,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -28,9 +28,10 @@ public class HooksController {
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "")
     })
-    public ResponseEntity identificationHook(@RequestBody(required = false) SolarisIdentification identification) {
-        log.info("Identification hook with content: " + identification);
-        savePersonIdentificationHandler.handle(identification);
+    public ResponseEntity identificationHook() {
+//    public ResponseEntity identificationHook(@RequestBody(required = false) SolarisIdentification identification) {
+        log.info("Identification hook with content: ");
+//        savePersonIdentificationHandler.handle(identification);
         return ResponseEntity.ok().build();
     }
 
@@ -44,5 +45,17 @@ public class HooksController {
         saveBookingHandler.handle(booking);
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/api/hooks/person")
+    @ApiOperation(value = "allow user to login, receives authorization token")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "")
+    })
+    public ResponseEntity personHook(@RequestParam(value = "person_id", required = false) String solarisId, @RequestParam(value = "status", required = false) String status) {
+        log.info("Booking hook with content: ");
+
+        return ResponseEntity.ok().build();
+    }
+
 
 }
