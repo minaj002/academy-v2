65002e5aac7c12fb3dba6196c1ad886d23a67a9b
Artis Jansons <artis.jansons@weststeincard.com>
2017-06-06 15:27:24 +0300

    Imports optimized

--- src/main/java/com/weststein/handler/person/GetPersonsHandler.java
+++ src/main/java/com/weststein/handler/person/GetPersonsHandler.java
@@ -2,21 +2,11 @@ package com.weststein.handler.person;
 
 import com.weststein.repository.Person;
 import com.weststein.repository.PersonRepository;
-import com.weststein.repository.Person_;
 import com.weststein.repository.specifications.PersonSpecifications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.util.StreamUtils;
 import org.springframework.stereotype.Component;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.Optional;
 
 @Component
 public class GetPersonsHandler {
