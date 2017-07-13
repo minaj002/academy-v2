925a2ab1059054065d471872a60dbe8170826df7
minaj002 <minaj002@gmail.com>
2017-07-13 15:10:25 +0300

    changed resource reader to spring

--- src/main/java/com/weststein/email/EmailTextSource.java
+++ src/main/java/com/weststein/email/EmailTextSource.java
@@ -1,11 +1,11 @@
 package com.weststein.email;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 @Component
 public class EmailTextSource {
@@ -13,8 +13,14 @@ public class EmailTextSource {
     public String getBody(String template, String language) {
 
         try {
-            FileReader reader = new FileReader(template);
-            BufferedReader br = new BufferedReader(reader);
+
+
+            ApplicationContext context =new ClassPathXmlApplicationContext();
+            Resource resource = context.getResource("email.html");
+
+//            FileReader reader = new FileReader(template);
+            BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+//            BufferedReader br = new BufferedReader(reader);
             String sCurrentLine = "";
 
             while (br.readLine() != null) {
